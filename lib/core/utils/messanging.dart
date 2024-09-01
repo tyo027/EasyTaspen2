864ffa22401e159f31c69e5174f31679e9792371
Arif Iskandar <arif19iskandar@gmail.com>
2024-09-01 15:56:18 +0700

    adding eception Handler

--- lib/core/utils/messanging.dart
+++ lib/core/utils/messanging.dart
@@ -1,34 +1,48 @@
 import 'package:easy/firebase_options.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 
 class Messanging {
   static bool _hasInit = false;
 
   static initialize() async {
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
+    try {
+      await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      );
 
-    FirebaseMessaging messaging = FirebaseMessaging.instance;
+      FirebaseMessaging messaging = FirebaseMessaging.instance;
 
-    await messaging.requestPermission(
-      alert: true,
-      announcement: false,
-      badge: true,
-      carPlay: false,
-      criticalAlert: false,
-      provisional: false,
-      sound: true,
-    );
+      await messaging.requestPermission(
+        alert: true,
+        announcement: false,
+        badge: true,
+        carPlay: false,
+        criticalAlert: false,
+        provisional: false,
+        sound: true,
+      );
 
-    _hasInit = true;
+      _hasInit = true;
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
   }
 
   static Future<String?> getToken() async {
-    if (!_hasInit) {
-      await initialize();
+    try {
+      if (!_hasInit) {
+        await initialize();
+      }
+      return FirebaseMessaging.instance.getToken();
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+      return null;
     }
-    return FirebaseMessaging.instance.getToken();
   }
 }
